7ad74a591670d419a33697881db21a49d66aa4ac
ajaystark <ajay18215@iiitd.ac.in>
2019-09-27 06:54:46 +0530

    changed system call

--- ProcessTaskStruct.c
+++ ProcessTaskStruct.c
@@ -3,23 +3,26 @@
 #include<linux/sched.h>
 #include<linux/syscalls.h>
 #include "process_task_struct.h"
-//#include "stdio.h"
-//#include <../include/linux/errno.h>
+#include <linux/module.h>
+#include <linux/fcntl.h>
+#include <asm/uaccess.h>
 asmlinkage long sys_listProcessInfo(long p_id, char* file_name) 
 {   
 
-     struct task_struct *proces;
+   struct task_struct *proces;
 
-  //  char output[1000];
-  // FILE *fptr;
+  int flag=0;
+  char* data;
 
-  // fptr = open(file_name,"w");
-  // if(fptr == NULL){
-  // printk("Error Reading file. Error no = o %d \n",errno); 
-  // return 0;
-   //}
-   int flag=0;
-    for_each_process(proces) {
+  struct file *file;
+  loff_t pos = 0;
+  int fd;
+  char buf[1];
+  mm_segment_t old_fs = get_fs();
+  set_fs(KERNEL_DS);
+
+
+  for_each_process(proces) {
  
    if((long)task_pid_nr(proces)==p_id){
 	
@@ -41,9 +44,8 @@ asmlinkage long sys_listProcessInfo(long p_id, char* file_name)
        (long)proces->static_prio, \
        (long)proces->normal_prio \
     );
- 
-/*
-fprintf(fptr,
+
+    sprintf(data,
       "Process: %s\n \
        PID_Number: %ld\n \
        Process State: %ld\n \
@@ -58,36 +60,35 @@ fprintf(fptr,
        (long)proces->rt_priority, \
        (long)proces->static_prio, \
        (long)proces->normal_prio \
-    );  
-*/  
-   if(proces->parent) 
-      printk(
-        "Parent process: %s, \
-         PID_Number: %ld", \ 
-         proces->parent->comm, \
-         (long)task_pid_nr(proces->parent) \
-      );
+    );
+
   
    printk("\n\n");
-/*
-fprint(fptr,
-        "Parent process: %s, \
-         PID_Number: %ld \n\n", \ 
-         proces->parent->comm, \
-         (long)task_pid_nr(proces->parent) \
-      );
-  
-   
-*/  
+
  }
-//  fclose(fptr);  
 
   }
 
 if(flag==0){
 printk("No process with PID %ld\n",p_id);
+sprintf(data,"No process with PID %ld\n",p_id);
 }
-  
+
+
+  fd = sys_open(file_name, O_WRONLY | O_CREATE , 0644);
+  if(fd>=0){
+    sys_write(fd,data,strlen(data));
+    file=fget(fd);
+    if(file){
+      vfs_write(file,data,strlen(data),&pos);
+      fput(file);
+    }
+    sys_close(fd);
+  }
+  else{
+    printk("Error in reading file.");
+  }
+  set_fs(old_fs);
 
   return 0;
 }
